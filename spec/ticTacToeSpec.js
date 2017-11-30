2882f89c709d9232abdc339ce7a8d30afc0a4284
ainsleybc <ab.chang@me.com>
2017-11-30 14:15:08 +0000

    added grid for gameplay

--- spec/ticTacToeSpec.js
+++ spec/ticTacToeSpec.js
@@ -3,16 +3,34 @@
 const TicTacToe = require('../src/ticTacToe.js').TicTacToe;
 
 describe('TicTacToe', () => {
+
+  var player1, player2, game, grid;
+
+  beforeEach(() => {
+    player1 = 'player1',
+    player2 = 'player2',
+    game    = new TicTacToe(player1, player2);
+  })
   
   describe('constructor', () => {
     
     it('initializes with two players', () => {
-      var player1 = 'player1',
-        player2   = 'player2',
-        game      = new TicTacToe(player1, player2);
       expect(game.players).toEqual([player1, player2]);
+    })
+    it('initializes with a game board', () => {
+      grid = [['', '', ''], ['', '', ''], ['', '', '']];
+      expect(game.grid).toEqual(grid);
     })
 
   })
+
+  // describe('cross', () => {
+    
+  //   it('allows a field to be marked with a cross', () => {
+
+  //     game.cross()
+  //   })
+
+  // })
 
 })
--- src/ticTacToe.js
+++ src/ticTacToe.js
@@ -2,7 +2,8 @@
 
 class TicTacToe {
   constructor(player1, player2) {
-    this.players = [player1, player2]
+    this.players = [player1, player2];
+    this.grid = Array(3).fill(Array(3).fill(''));
   }
 }
 
